cfedf260a4c733889a531d8f8c05b5e34bdbde27
lufly9 <126124578+lufly9@users.noreply.github.com>
2024-06-22 14:20:37 +0800

    Add files via upload

---
+++ user_config.py
@@ -0,0 +1,31 @@
+source_file = "demo.txt"
+final_file = "result.txt"
+favorite_list = [
+    "东方卫视高清",
+    "CCTV-1高清",
+    "CCTV-2高清",
+    "CCTV-5+高清",
+    "CCTV-13高清",
+    "凤凰中文",
+    "湖南卫视高清",
+    "凤凰资讯",
+    "咪咕直播 1",
+]
+open_online_search = False
+favorite_page_num = 3
+default_page_num = 3
+urls_limit = 5
+open_sort = False
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://raw.githubusercontent.com/lufly9/ZBTV/main/result.txt",
+    "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
+]
+open_multicast = False
+region_list = ["all"]
